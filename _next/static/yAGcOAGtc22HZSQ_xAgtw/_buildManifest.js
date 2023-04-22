d6d34e0a268cac614bb5aea18752b25471022fc5
-Taha Dostifam‍- <mr.tahadostifam@gmail.com>
2023-04-22 16:29:59 +0330

    Updates

---
+++ _next/static/yAGcOAGtc22HZSQ_xAgtw/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/3f909ba5d96efd38.css","static/chunks/pages/index-f11017f6a7b26bb3.js"],"/_error":["static/chunks/pages/_error-bcd56d9b40128cc1.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
