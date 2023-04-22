319c0f612bb0a983f292c03b458e30756f6b7870
-Taha Dostifam‍- <mr.tahadostifam@gmail.com>
2023-04-22 16:43:31 +0330

    Fix basePath

--- next.config.js
+++ next.config.js
@@ -1,6 +1,16 @@
 /** @type {import('next').NextConfig} */
+
+const SITE_URL = process.env.NODE_ENV === 'production' ? "/Client" : "/";
+
 const nextConfig = {
   reactStrictMode: true,
-}
+  output: "export",
+  distDir: "dist",
+  trailingSlash: true,
+  images: {
+    unoptimized: true
+  },
+  basePath: SITE_URL
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
